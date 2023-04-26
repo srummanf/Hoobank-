b1cf85694a4d86286ce37eb46e36046d29f386cc
Rumman <rummanfardeen4567@gmail.com>
2023-04-26 22:54:41 +0530

    First Commit

---
+++ Hoobank/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-primary">
+      <h1 className="bg-secondary">Hoobank</h1>
+    </div>
+  )
+};
+
+export default App
