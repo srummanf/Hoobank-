6e4b5c5100b1d87ef612a717838f149958367dba
Rumman <rummanfardeen4567@gmail.com>
2023-04-27 09:46:13 +0530

    Files Added

--- Hoobank/src/App.jsx
+++ Hoobank/src/App.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import styles from './style';
+import { Navbar, Hero, Billing, Stats, Business, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+
 
 const App = () => {
   return (
-    <>    
-    <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`} >
-        <div className={`${styles.boxWidth}`}>
-          Navbar
+    <>
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`} >
+          <div className={`${styles.boxWidth}`}>
+            Navbar
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billinf
-        CardDeals
-        Testimonial
-        ClientsCTA
-        Footer
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+
       </div>
-
-    </div>
     </>
   )
 };
