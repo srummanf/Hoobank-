b92e18159f4ad5a92176bbbb486b53c5fe2d9695
Rumman <rummanfardeen4567@gmail.com>
2023-04-27 10:11:14 +0530

    Files Added

--- Hoobank/src/App.jsx
+++ Hoobank/src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
+          <Navbar />
           <Stats />
           <Business />
           <Billing />
