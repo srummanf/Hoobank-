dc91448a56785c3179a51bb365636af5ecdc6b2e
Rumman <rummanfardeen4567@gmail.com>
2023-04-27 09:32:23 +0530

    Files Added

--- Hoobank/src/App.jsx
+++ Hoobank/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import styles from './style';
 
 const App = () => {
   return (
-    <div className="bg-primary">
-      <h1 className="bg-secondary">Hoobank</h1>
+    <>    
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`} >
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
     </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billinf
+        CardDeals
+        Testimonial
+        ClientsCTA
+        Footer
+      </div>
+
+    </div>
+    </>
   )
 };
 
