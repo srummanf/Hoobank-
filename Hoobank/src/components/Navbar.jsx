2c92c2fa17571f64b69d78d7cb44cadaeb1656da
Rumman <rummanfardeen4567@gmail.com>
2023-04-28 18:54:56 +0530

    First Commit

--- Hoobank/src/components/Navbar.jsx
+++ Hoobank/src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
   return (
     <>
       <nav className="w-full flex py-6 justify-between items-center navbar">
@@ -11,13 +13,18 @@ const Navbar = () => {
         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
           {navLinks.map((nav,index) => {
             <li
-              key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'}`}>
+              key={nav.id} 
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'}`}>
               <a href={`#${nav.id}`}>
                 {nav.title}
               </a>
             </li>
-          })};
+          })}
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu}></img>
+        </div>
       </nav>
     </>
 
